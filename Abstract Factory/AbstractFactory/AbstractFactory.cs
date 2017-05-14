41f406a968c7741b1cbfbb88187fb85d5a9e790a
VanHakobyan <van19962013@mail.ru>
2017-05-14 19:24:47 +0400

    add first creating :one:

---
+++ Abstract Factory/AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    abstract class AbstractFactory
+    {
+        public abstract AbstractBottle CreateBottle();
+        public abstract AbstractWater CreateWater();
+    }
+}
---
+++ Abstract Factory/AbstractFactory/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Client
+    {
+        private AbstractWater water;
+        private AbstractBottle bottle;
+        public Client(AbstractFactory factory)
+        {
+            water = factory.CreateWater();
+            bottle = factory.CreateBottle();
+        }
+        public void Run()
+        {
+            bottle.Interact(water);
+        }
+    }
+}
