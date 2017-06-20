3cecbf782f2a03e2717da32762e8b78b59ab0bd3
VanHakobyan <van19962013@mail.ru>
2017-06-20 22:41:43 +0400

    add foreman :+1:

--- Builder/Builder/Pattern/Builder/Builder.cs
+++ Builder/Builder/Pattern/Builder/Builder.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Builder.House;
+﻿using Builder.House;
 
 namespace Builder.Pattern.Builder
 {
---
+++ Builder/Builder/Pattern/Foreman.cs
@@ -0,0 +1,13 @@
+﻿namespace Builder.Pattern
+{
+    class Foreman
+    {
+        Builder.Builder builder = new Builder.Builder();
+        public void ConstructHouse()
+        {
+            builder.BuildBasement();
+            builder.BuildStorey();
+            builder.BuildRoof();
+        }
+    }
+}
