0bff8458865666f1f4e23c53438ac03a5a07253f
VanHakobyan <van19962013@mail.ru>
2017-06-20 22:46:02 +0400

    add  Program :book:

--- Builder/Builder/Program.cs
+++ Builder/Builder/Program.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Builder
+﻿namespace Builder
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Pattern.Builder.AbstractBuilder builder = new Pattern.Builder.Builder();
+            Pattern.Foreman foreman = new Pattern.Foreman();
+            foreman.ConstructHouse();
+
+            House.House house = builder.GetResulit();
         }
     }
 }
