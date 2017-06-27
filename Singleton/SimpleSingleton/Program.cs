db91110a27f1ff652e813c8c0f32d53b76442199
VanHakobyan <van19962013@mail.ru>
2017-06-27 13:28:22 +0400

    add simple  project singlton  :basketball:

---
+++ Singleton/SimpleSingleton/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleSingleton
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton inst1 = Singleton.Instance();
+            Singleton inst2 = Singleton.Instance();
+            Console.WriteLine(inst1.Equals(inst2));           //true
+            Console.WriteLine(ReferenceEquals(inst1, inst2));  //true
+        }
+    }
+}
---
+++ Singleton/SimpleSingleton/Singleton.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleSingleton
+{
+    class Singleton
+    {
+        static Singleton uniqueValue;
+        private Singleton()
+        {
+        }
+        public static Singleton Instance()
+        {
+            if (uniqueValue == null)
+                uniqueValue = new Singleton();
+            return uniqueValue;
+        }
+    }
+}
