a271ef227e3e00c65308a1e8a1b7c0e6ad747ab3
vanhakobyan <van19962013@mail.ru>
2017-07-04 20:26:37 +0400

    add Explanation :ok:

--- Composite/CompositeTree/Program.cs
+++ Composite/CompositeTree/Program.cs
@@ -1,28 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CompositeTree
+﻿namespace CompositeTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
             Component root = new Composite("root");
             Component branch1 = new Composite("br1");
             Component branch2 = new Composite("br2");
             Component leaf1 = new Leaf("leaf1");
             Component leaf2 = new Leaf("leaf2");
+            Component leaf3 = new Leaf("leaf3");
+
+            //Create tree
 
             root.Add(branch1);
             root.Add(branch2);
+
+            //           *           //Root
+            //        /     \
+            //       *       *       //branch1 and branch2
+
             branch1.Add(leaf1);
             branch2.Add(leaf2);
-            root.Operation();
+            branch1.Add(leaf3);
+
+            //           *           //Root
+            //        /     \       
+            //       *       *       //branch1 and branch2
+            //     / |        \
+            //    ~  ~         ~     // leaf1,leaf3 and leaf2
+            //
+            //
+            root.Operation();        // Creating
         }
     }
 }
