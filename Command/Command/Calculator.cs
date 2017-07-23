16968ce42472c28f2d25deaf758f230461035d51
vanhakobyan <van19962013@mail.ru>
2017-07-23 15:23:28 +0400

    add Calculator :v:

---
+++ Command/Command/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using Command.ConcreteCommand;
+
+namespace Command
+{
+    class Calculator
+    {
+        ControleUnit cUnit;
+        ArithmeticUnit aUnit;
+        public Calculator()
+        {
+            cUnit = new ControleUnit();
+            aUnit = new ArithmeticUnit();
+        }
+        public int Run(AbstractCommand command)
+        {
+            cUnit.ExecuteCommand();
+            cUnit.StoreCommand(command);
+            return aUnit.Register;
+        }
+        public int Add(int operand)
+        {
+            return Run(new Add(aUnit, operand));
+        }
+        public int Sub(int operand)
+        {
+            return Run(new Sub(aUnit, operand));
+        }
+        public int Mul(int operand)
+        {
+            return Run(new Mul(aUnit, operand));
+        }
+        public int Div(int operand)
+        {
+            return Run(new Div(aUnit, operand));
+        }
+
+    }
+}
