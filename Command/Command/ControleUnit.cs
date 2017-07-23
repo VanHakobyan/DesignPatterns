bafa8d91870256bb9efa4deba1bb28160d37aca6
vanhakobyan <van19962013@mail.ru>
2017-07-23 15:16:40 +0400

    add Conreole Unit :one:

---
+++ Command/Command/ControleUnit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Command
+{
+    class ControleUnit
+    {
+        private List<AbstractCommand> commands = new List<AbstractCommand>();
+        private int current = 0;
+        public void StoreCommand(AbstractCommand com)
+        {
+            commands.Add(com);
+        }
+        public void ExecuteCommand()
+        {
+            commands[current].Execute();
+            current++;
+        }
+        public void Undo(int level)
+        {
+            for (int i = 0; i < level; i++)
+                if (current > 0)
+                    commands[--current].UnExecute();
+        }
+        public void Redo(int level)
+        {
+            for (int i = 0; i < level; i++)
+                if (current < commands.Count - 1)
+                    commands[++current].Execute();
+        }
+    }
+}
