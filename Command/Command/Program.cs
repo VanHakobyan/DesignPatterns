440b0c84d08a0b77f8be258563502497b096f6dd
vanhakobyan <van19962013@mail.ru>
2017-07-23 15:26:59 +0400

    program implemeted

--- Command/Command/Program.cs
+++ Command/Command/Program.cs
@@ -10,6 +10,20 @@ namespace Command
     {
         static void Main(string[] args)
         {
+            var calculator = new Calculator();
+            int resulit;
+
+            resulit = calculator.Add(5);
+            Console.WriteLine("resulit is "+ resulit);
+
+            resulit = calculator.Sub(2);
+            Console.WriteLine("resulit is " + resulit);
+
+            resulit = calculator.Div(2);
+            Console.WriteLine("resulit is " + resulit);
+
+            resulit = calculator.Mul(12);
+            Console.WriteLine("resulit is " + resulit);
         }
     }
 }
